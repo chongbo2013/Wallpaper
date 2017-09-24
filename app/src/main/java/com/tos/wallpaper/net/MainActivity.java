781b4964b02390b46bc4be5d9326f84c4e487641
459821731@qq.com <11111111abcde>
2017-09-25 00:12:32 +0800

    ddddd
    Signed-off-by: 459821731@qq.com <11111111abcde>

--- app/src/main/java/com/tos/wallpaper/net/MainActivity.java
+++ app/src/main/java/com/tos/wallpaper/net/MainActivity.java
@@ -4,12 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+
 public class MainActivity extends AppCompatActivity {
     ImageView iv;
+    String img="https://raw.githubusercontent.com/chongbo2013/Wallpaper/master/app/src/main/assets/wallpaper/wallpaper1/wallpaper.png";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         iv= (ImageView) findViewById(R.id.iv);
+        //
+        Glide.with(this)
+                .load(img)  //可以从本地图片资源读取，也可以从Url去加载。
+                .crossFade()//显示渐变效果
+                .into(iv);
     }
 }

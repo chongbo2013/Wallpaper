a480724e006a240c49cb3778b8bdf015ffb93b71
459821731@qq.com <11111111abcde>
2017-09-24 21:11:51 +0800

    ddd

--- app/src/main/java/com/tos/wallpaper/net/MainActivity.java
+++ app/src/main/java/com/tos/wallpaper/net/MainActivity.java
@@ -2,12 +2,14 @@ package com.tos.wallpaper.net;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iv= (ImageView) findViewById(R.id.iv);
     }
 }
